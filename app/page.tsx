0ce4511bf74538bb9cae0f386503585c59acdd20
Silas <chibukesilas@gmail.com>
2025-11-24 04:10:00 +0100

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,60 @@
+import Link from "next/link";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="h-screen flex flex-col items-center justify-center bg-black relative px-6 w-screen overflow-hidden">
+      <div className="w-8 h-8 rounded-md bg-primary flex items-center justify-center fontBoskaBlack text-[#121212] z-20">
+        Ze
+      </div>
+      <div className="text-7xl font-bold text-secondary my-6 fontBoska leading-20 text-center z-20">
+        <span className="block text-xl fontBoskaitalic">A New</span>
+        <h1 className="block ">
+          Era of <span className="text-primary">Style</span>
+        </h1>
+        <span className="block text-2xl fontBoskaitalic">Begins Soon.</span>
+      </div>
+      <p className="text-neutral-300 text-center text-sm mb-8 z-20">
+        Be among the first to experience the launch of our exclusive fashion
+        brand.
+      </p>
+      <div className="flex flex-col gap-4 w-full max-w-sm items-center z-20">
+        <div className="w-full border px-4 py-2 rounded-full relative bg-[#f3f3f3]">
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Your Email"
+            className="outline-none text-black"
+          />
+          <div className="absolute top-1 right-1 w-fit bg-primary shadow p-2 rounded-full text-xs ">
+            Get your Free Ticket
+          </div>
+        </div>
+        <Link
+          href={"/"}
+          className="fontBoskaitalic text-primary text-sm hover:scale-105 transition-all duration-300 hover:underline"
+        >
+          -Join the WaitList-
+        </Link>
+      </div>
+      <div className="absolute w-[300px] h-[600px] -left-[134px] -bottom-[350px] scale-[4] z-10 max-sm:hidden">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+          src={"/model5.png"}
+          alt="Model Image"
+          fill
+          className="object-cover filter blur-[1px] grayscale-100 hover:grayscale-0 transition"
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div>
+
+      <div className="absolute w-[300px] h-[600px] -right-32 -bottom-8 max-sm:hidden">
+        <Image
+          src={"/model4.png"}
+          alt="Model Image"
+          fill
+          className="object-cover filter blur-[1px] grayscale-100 hover:grayscale-0 transition"
+        />
+      </div>
     </div>
   );
 }
